ac39367ba57d651e5a7fae2b192dbc25346a2a8b
kondoumh <kondoumh@gmail.com>
2019-01-15 00:09:04 +0900

    add if let sample

--- use_enum/src/main.rs
+++ use_enum/src/main.rs
@@ -38,4 +38,13 @@ fn main() {
     let five = Some(5);
     let six = plus_one(five);
     let none = plus_one(None);
+
+    let some_u8_value = Some(0u8);
+    match some_u8_value {
+        Some(3) => println!("three"),
+        _ => (),
+    }
+    if let Some(3) = some_u8_value {
+        println!("three");
+    }
 }

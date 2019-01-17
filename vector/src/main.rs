cd01808562d7549a9a9f9d835bbd74f76e8b3e76
kondoumh <kondoumh@gmail.com>
2019-01-18 00:03:29 +0900

    add vector operation sample

--- vector/src/main.rs
+++ vector/src/main.rs
@@ -1,11 +1,35 @@
+enum SpreadSheetCell {
+    Int(i32),
+    Float(f64),
+    Text(String),
+}
+
 fn main() {
     let v:Vec<i32> = Vec::new();
 
-    let v = vec![1, 2, 3];
+    let v = vec![1, 2, 3, 4];
 
     let mut v = Vec::new();
     v.push(5);
     v.push(6);
     v.push(7);
     v.push(8);
+
+    let third: &i32 = &v[2];
+    let third: Option<&i32> = v.get(2);
+
+    for i in &v {
+        println!("{}", i);
+    }
+
+    let mut v = vec![1, 2, 3];
+    for i in &mut v {
+        *i += 100;
+    }
+
+    let row = vec![
+        SpreadSheetCell::Int(3),
+        SpreadSheetCell::Text(String::from("blue")),
+        SpreadSheetCell::Float(10.12),
+    ];
 }

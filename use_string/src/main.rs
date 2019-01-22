b7143cdad626ca79a03e860b451b49dba653e5bb
kondoumh <kondoumh@gmail.com>
2019-01-23 06:34:38 +0900

    string example

--- use_string/src/main.rs
+++ use_string/src/main.rs
@@ -6,4 +6,10 @@ fn main() {
     let s = "initial contents".to_string();
 
     let s = String::from("initial contents");
+
+    let mut s = String::from("foo");
+    s.push_str("bar");
+    let s2 = "bar";
+    s.push_str(s2);
+    println!("s2 is {}", s2);
 }

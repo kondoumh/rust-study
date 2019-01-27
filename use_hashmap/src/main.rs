29470a3fbc893a2bc5926ae7812092eaac51938b
kondoumh <kondoumh@gmail.com>
2019-01-28 06:58:21 +0900

    add HashMap sample

--- use_hashmap/src/main.rs
+++ use_hashmap/src/main.rs
@@ -5,4 +5,8 @@ fn main() {
 
     scores.insert(String::from("Blue"), 10);
     scores.insert(String::from("Yellow"), 50);
+
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_score = vec![10, 50];
+    let scores: HashMap<_, _> = teams.iter().zip(initial_score.iter()).collect();
 }

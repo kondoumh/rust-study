9059b7313445653e8b4faa022926198fcf664b6f
kondoumh <kondoumh@gmail.com>
2019-01-27 11:32:25 +0900

    add string example

--- use_string/src/main.rs
+++ use_string/src/main.rs
@@ -12,4 +12,31 @@ fn main() {
     let s2 = "bar";
     s.push_str(s2);
     println!("s2 is {}", s2);
+
+    let mut s = String::from("lo");
+    s.push('l');
+    println!("{}", s);
+
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world");
+    let s3 = s1 + &s2;
+    let s4 = s2;
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    //let s = s1 + "-" + &s2 + "-" + &s3;
+    let s = format!("{}-{}-{}", s1, s2, s3);
+
+    let hello = "Здравствуйте";
+    println!("{}", &hello[0..4]);
+
+    for c in "नमस्ते".chars() {
+        println!("{}", c);  
+    }
+
+    for b in "नमस्ते".bytes() {
+        println!("{}", b);
+    }
 }

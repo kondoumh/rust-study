31e644dfa771fa61ef3bed5dd534a1a32c6414d8
kondoumh <kondoumh@gmail.com>
2019-01-30 14:42:28 +0900

    add generics example

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -9,6 +9,37 @@ fn largest<T>(list: &[T]) -> T {
     largest
 }
 
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+struct Point2<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point2<T, U> {
+    fn mixup<V, W>(self, other:Point<V, W>) -> Point<T, W> {
+        Point2 {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
     let result = largest(&number_list);
@@ -17,4 +48,13 @@ fn main() {
     let char_list = vec!['y', 'm', 'a', 'q'];
     let result = largest(&char_list);
     println!("The largest char is {}", result);
+
+    let ipt = Point { x: 5, y: 10 };
+    let fpt = Point { x: 1.0, y: 4.0 };
+    println!("p.x = {}", p.x());
+
+    let p1 = Point2 { x: 5, y: 10.4 };
+    let p2 = Point2 { x: "Hello", y: "c"};
+    pet p3 = p1.mixup(p2);
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }

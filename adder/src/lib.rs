f097996247ee6434754d12ee3a82b8c9d3e01266
kondoumh <kondoumh@gmail.com>
2019-02-03 08:35:45 +0900

    add product code

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,12 +1,13 @@
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn exploration() {
-        assert_eq!(2 + 2, 4);
-    }
+    use super::*;
 
     #[test]
-    fn another() {
-        panic!("Make this test fail");
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
     }
 }

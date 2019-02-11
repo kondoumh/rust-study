e7d956d4f67d47d6d73dfac6e9547e2f29547559
kondoumh <kondoumh@gmail.com>
2019-02-11 13:20:34 +0900

    add search function

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -29,6 +29,18 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
     Ok(())
 }
 
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -51,5 +63,18 @@ mod test {
                 assert!(e.contains("not enough"));
             }
         }
+    }
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        )
     }
 }

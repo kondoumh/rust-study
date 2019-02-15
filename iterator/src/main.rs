1fdde617c2f2420d1c39228dcd27a73025a1e6d5
kondoumh <kondoumh@gmail.com>
2019-02-16 07:54:36 +0900

    add specific iterator

--- iterator/src/main.rs
+++ iterator/src/main.rs
@@ -10,6 +10,30 @@ fn shoes_im_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
         .collect()
 }
 
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+
+        if self.count < 6 {
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
 fn main() {
     let v1: Vec<i32> = vec![1, 2, 3];
     let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
@@ -44,5 +68,26 @@ mod tests {
                 Shoe { size: 10, style: String::from("boot") },
             ]
         );
+    }
+
+    #[test]
+    fn calling_next_directly() {
+        let mut counter = Counter::new();
+        assert_eq!(counter.next(), Some(1));
+        assert_eq!(counter.next(), Some(2));
+        assert_eq!(counter.next(), Some(3));
+        assert_eq!(counter.next(), Some(4));
+        assert_eq!(counter.next(), Some(5));
+        assert_eq!(counter.next(), None);
+    }
+
+    #[test]
+    fn using_other_iterator_torin_method() {
+        let sum: u32 = Counter::new().zip(Counter::new().skip(1))
+                                     .map(|(a, b) | a * b)
+                                     .filter(|x| x % 3 == 0)
+                                     .sum();
+        
+        assert_eq!(18, sum);
     }
 }

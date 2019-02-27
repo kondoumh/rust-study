ec7b4fb2420bb2b19ae28f570c8cd980e0a91418
kondoumh <kondoumh@gmail.com>
2019-02-28 07:01:13 +0900

    implement Deref trait

--- smartpointer/src/main.rs
+++ smartpointer/src/main.rs
@@ -3,14 +3,51 @@ enum List {
     Nil,
 }
 
-use List::{Cons, Nil};
+// use List::{Cons, Nil};
+
+use std::ops::Deref;
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
 
 fn main() {
     let b = Box::new(5);
     println!("b = {}", b);
 
-    let list = Cons(1,
-        Box::new(Cons(2,
-            Box::new(Cons(3,
-                Box::new(Nil))))));
+    // let list = Cons(1,
+    //     Box::new(Cons(2,
+    //         Box::new(Cons(3,
+    //             Box::new(Nil))))));
+
+    let x = 5;
+    let y = &x;
+    let z = Box::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+    assert_eq!(5, *z);
+
+    let m = MyBox::new(x);
+
+    assert_eq!(5, *m);
+
+    let n = MyBox::new(String::from("Rust"));
+    hello(&n);
+}
+
+fn hello(name: &str) {
+    println!("Hello, {}!", name);
 }

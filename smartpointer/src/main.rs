9fa124024a9d6bc2129b686b4c748946fac1f175
kondoumh <kondoumh@gmail.com>
2019-02-28 15:42:49 +0900

    apply Rc

--- smartpointer/src/main.rs
+++ smartpointer/src/main.rs
@@ -1,9 +1,10 @@
 enum List {
-    Cons(i32, Box<List>),
+    Cons(i32, Rc<List>),
     Nil,
 }
 
 use List::{Cons, Nil};
+use std::rc::Rc;
 
 use std::ops::Deref;
 
@@ -37,10 +38,10 @@ fn main() {
     let b = Box::new(5);
     println!("b = {}", b);
 
-    let list = Cons(1,
-        Box::new(Cons(2,
-            Box::new(Cons(3,
-                Box::new(Nil))))));
+    // let list = Cons(1,
+    //     Box::new(Cons(2,
+    //         Box::new(Cons(3,
+    //             Box::new(Nil))))));
 
     let x = 5;
     let y = &x;
@@ -63,11 +64,17 @@ fn main() {
     drop(c);
     println!("CustomSmartPointer dropped before the end of main.");
 
-    // let a = Cons(5,
-    //     Box::new(Cons(10,
-    //         Box::new(Nil))));
-    // let b = Cons(3, Box::new(a));
-    // let c = Cons(4, Box::new(a));
+    let a = Rc::new(Cons(5,
+         Rc::new(Cons(10,
+             Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
 
 fn hello(name: &str) {

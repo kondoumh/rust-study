fe49a2566d77756e2f43cf32c2f839cd91d6266b
kondoumh <kondoumh@gmail.com>
2019-03-12 01:03:36 +0900

    add rc

--- smartpointer3/src/main.rs
+++ smartpointer3/src/main.rs
@@ -20,4 +20,7 @@ impl List {
 fn main() {
     let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
     println!("a initial rc count = {}", Rc::strong_count(&a));
+    println!("a next item = {:?}", a.tail());
+    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+    println!("a rc count after b creation = {}", Rc::strong_count(&a));
 }

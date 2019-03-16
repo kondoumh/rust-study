6ba41abcae89925b7b82cdaa46713a7c18e034f2
kondoumh <kondoumh@gmail.com>
2019-03-16 14:52:21 +0900

    add parent node with weak reference

--- smartpointer3/src/main.rs
+++ smartpointer3/src/main.rs
@@ -1,6 +1,7 @@
 use std::rc::Rc;
 use std::cell::RefCell;
 use List::{Cons, Nil};
+use std::rc::Weak;
 
 #[derive(Debug)]
 enum List {
@@ -20,6 +21,7 @@ impl List {
 #[derive(Debug)]
 struct Node {
     value: i32,
+    parent: RefCell<Weak<Node>>,
     children: RefCell<Vec<Rc<Node>>>,
 }
 
@@ -42,11 +44,16 @@ fn main() {
 
     let leaf = Rc::new(Node {
         value: 3,
+        parent: RefCell::new(Weak::new()),
         children: RefCell::new(vec![]),
     });
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
 
     let branch = Rc::new(Node {
         value: 5,
+        parent: RefCell::new(Weak::new()),
         children: RefCell::new(vec![Rc::clone(&leaf)]),
     });
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
 }

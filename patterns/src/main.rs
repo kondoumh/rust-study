31f1c14608816def9e84814c0c10cf6ce1c4ea73
kondoumh <kondoumh@gmail.com>
2019-03-26 00:23:30 +0900

    add patterns

--- patterns/src/main.rs
+++ patterns/src/main.rs
@@ -58,6 +58,26 @@ fn main() {
         _ => println!("Default case, x = {:?}", x),
     }
     println!("at the end: x = {:?}, y = {:?}", x, y);
+
+    let x = 1;
+    match x {
+        1 | 2 => println!("one or two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+
+    let x = 5;
+    match x {
+        1 ... 5 => println!("one through five"),
+        _ => println!("something else"),
+    }
+
+    let x = 'c';
+    match x {
+        'a' ... 'j' => println!("early ASCII letter"),
+        'k' ... 'z' => println!("late ASCII letter"),
+        _ => println!("something else"),
+    }
 }
 
 fn print_coorinates(&(x, y): &(i32, i32)) {

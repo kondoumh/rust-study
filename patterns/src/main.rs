c368f2eba0f67ba39c86ada2f53dd4561ffb9a21
kondoumh <kondoumh@gmail.com>
2019-03-28 12:59:13 +0900

    add pattern

--- patterns/src/main.rs
+++ patterns/src/main.rs
@@ -3,6 +3,13 @@ struct Point {
     y: i32,
 }
 
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
 fn main() {
     let favorite_color: Option<&str> = None;
     let is_tuesday = false;
@@ -93,6 +100,39 @@ fn main() {
     let Point { x: a, y: b} = p;
     assert_eq!(0, a);
     assert_eq!(7, b);
+
+    let msg = Message::ChangeColor(0, 160, 255);
+    match msg {
+        Message::Quit => {
+            println!("The Quit variant has no data to destructure")
+        },
+        Message::Move { x, y } => {
+            println!(
+                "Move in the x direction {} and in the y direction {}",
+                x,
+                y,
+            );
+        },
+        Message::Write(text) => println!("Text message: {}", text),
+        Message::ChangeColor(r, g, b) => {
+            println!(
+                "change the color to red {}, green {}, and blue {}",
+                r,
+                g,
+                b
+            )
+        },
+    }
+
+    let points = vec![
+        Point { x: 0, y: 0 },
+        Point { x: 1, y: 5 },
+        Point { x: 10, y: -3 },
+    ];
+    let sum_of_sqares: i32 = points
+        .iter()
+        .map(|&Point { x, y }| x * x + y * y)
+        .sum();
 }
 
 fn print_coorinates(&(x, y): &(i32, i32)) {

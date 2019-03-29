f68a56705324414097b031b255510a13ebb165f0
kondoumh <kondoumh@gmail.com>
2019-03-29 17:32:09 +0900

    add pattern

--- patterns/src/main.rs
+++ patterns/src/main.rs
@@ -10,6 +10,12 @@ enum Message {
     ChangeColor(i32, i32, i32),
 }
 
+struct Point3d {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+
 fn main() {
     let favorite_color: Option<&str> = None;
     let is_tuesday = false;
@@ -137,6 +143,36 @@ fn main() {
     let ((feet, inches), Point {x, y}) = ((3, 10), Point { x: 3, y: -10 });
 
     foo(3, 4);
+
+    let mut setting_value = Some(5);
+    let new_setting_value = Some(10);
+    match (setting_value, new_setting_value) {
+        (Some(_), Some(_)) => {
+            println!("Can't overwrite an existing customized value");
+        }
+        _ => {
+            setting_value = new_setting_value;
+        }
+    }
+    println!("setting is {:?}", setting_value);
+
+    let numbers = (2, 4, 8, 16, 32);
+    match numbers {
+        (first, _, third, _, fifth) => {
+            println!("Some numbers: {}, {}, {}", first, third, fifth)
+        },
+    }
+
+    let origin = Point3d { x: 0, y: 0, z: 0 };
+    match origin {
+        Point3d { x, .. } => println!("x is {}", x),
+    }
+
+    match numbers {
+        (first, .., last) => {
+            println!("Some numbers: {}, {}", first, last);
+        },
+    }
 }
 
 fn print_coorinates(&(x, y): &(i32, i32)) {

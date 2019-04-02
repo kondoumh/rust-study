f48333c84bb1ef31c46a8479995ca530f950923f
kondoumh <kondoumh@gmail.com>
2019-04-02 12:38:18 +0900

    add lifetime exampe project

---
+++ lifetime_advanced/src/lib.rs
@@ -0,0 +1,19 @@
+struct Context<'a>(&'a str);
+
+struct Parser<'a> {
+    context: &'a Context<'a>,
+}
+
+impl<'a> Parser<'a> {
+    fn parse(&self) -> Result<(), &str> {
+        Err(&self.context.0[1..])
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}

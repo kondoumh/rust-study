30a3e4b2a0552675ff9281ece35d1dca67430468
kondoumh <kondoumh@gmail.com>
2019-04-08 12:12:21 +0900

    add full path notation example

--- traits_advanced/src/main.rs
+++ traits_advanced/src/main.rs
@@ -28,9 +28,42 @@ impl Add<Meters> for Millimeters {
     }
 }
 
+trait Pilot {
+    fn fly(&self);
+}
+
+trait Wizard {
+    fn fly(&self);
+}
+
+struct Human;
+
+impl Pilot for Human {
+    fn fly(&self) {
+        println!("This is your captain speaking.");
+    }
+}
+
+impl Wizard for Human {
+    fn fly(&self) {
+        println!("Up!");
+    }
+}
+
+impl Human {
+    fn fly(&self) {
+        println!("*waving arms furiously*");
+    }
+}
+
 fn main() {
     assert_eq!(Point { x: 1, y: 0} + Point { x: 2, y: 3},
                Point { x: 3, y: 3});
 
     assert_eq!(Millimeters(200) + Meters(3), Millimeters(3200));
+
+    let person = Human;
+    Pilot::fly(&person);
+    Wizard::fly(&person);
+    person.fly();
 }

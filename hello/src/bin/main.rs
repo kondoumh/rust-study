c652a489c38eb062ed9395915ae5594df926f31e
kondoumh <kondoumh@gmail.com>
2019-04-14 22:59:37 +0900

    WIP add ThreadPool

--- hello/src/bin/main.rs
+++ hello/src/bin/main.rs
@@ -1,3 +1,5 @@
+extern crate hello;
+use hello::ThreadPool;
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
@@ -7,9 +9,10 @@ use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        thread::spawn(|| {
+        pool.execute(|| {
             handle_connection(stream);
         });
     }

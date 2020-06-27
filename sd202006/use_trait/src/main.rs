32c7e1b3e39344f752cab2d31c508703f1f5d0ea
kondoumh <kondoumh@gmail.com>
2020-06-27 15:09:47 +0900

    apply trait

--- sd202006/use_trait/src/main.rs
+++ sd202006/use_trait/src/main.rs
@@ -3,30 +3,38 @@ struct RightTriangle {
     perpendicular: f64,
 }
 
-impl RightTriangle {
+struct Rectangle {
+    width: f64,
+    height: f64,
+}
+
+trait GeoCalculator {
+    fn area(&self) -> f64;
+    fn length(&self) -> f64;
+}
+
+impl GeoCalculator for RightTriangle {
     fn area(&self) -> f64 {
         (self.base * self.perpendicular) * 0.5
     }
-
     fn length(&self) -> f64 {
         self.base + self.perpendicular
             + (self.base.powi(2) + self.perpendicular.powi(2)).sqrt()
     }
 }
 
-struct Rectangle {
-    width: f64,
-    height: f64,
-}
-
-impl Rectangle {
+impl GeoCalculator for Rectangle {
     fn area(&self) -> f64 {
         self.width * self.height
     }
-
     fn length(&self) -> f64 {
         (self.width + self.height) * 2.0
     }
+}
+
+fn printval<T: GeoCalculator>(poly: &T) {
+    println!("{}", poly.area());
+    println!("{}", poly.length());
 }
 
 fn main() {
@@ -34,13 +42,11 @@ fn main() {
         base: 3.0,
         perpendicular: 4.0,
     };
-    println!("{}", tri.area());
-    println!("{}", tri.length());
+    printval(&tri);
 
     let rec = Rectangle {
         width: 3.0,
         height: 4.0,
     };
-    println!("{}", rec.area());
-    println!("{}", rec.length());
+    printval(&rec);
 }

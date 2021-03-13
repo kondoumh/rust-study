e589261a8af1a7db4ba3faa14c7d29b432ddbadf
kondoumh <kondoumh@gmail.com>
2021-03-13 15:51:58 +0900

    Add wasm_bidndgen fn

--- sd-202103-intro-wasm/src/lib.rs
+++ sd-202103-intro-wasm/src/lib.rs
@@ -1,7 +1,31 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use photon_rs::{filters::filter, PhotonImage};
+use rand::seq::SliceRandom;
+use wasm_bindgen::prelude::wasm_bindgen;
+
+const FILTERS: &[&str] = &[
+    "oceanic",
+    "islands",
+    "marine",
+    "seagreen",
+    "flagblue",
+    "liquid",
+    "diamante",
+    "radio",
+    "twenties",
+    "rosetint",
+    "mauve",
+    "bluechrome",
+    "vintage",
+    "perfume",
+    "serenity",
+];
+
+#[wasm_bindgen]
+pub fn apply_some_filter(mut img: &mut PhotonImage) -> String {
+    let mut rng = rand::thread_rng();
+    let chosen = FILTERS
+        .choose(&mut rng)
+        .expect("Can't choose a filter name!");
+    filter(&mut img, chosen);
+    chosen.to_string()
 }

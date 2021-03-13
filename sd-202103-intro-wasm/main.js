dca7ff50bd7fdd461572d775adc4ac58b5dbfb92
kondoumh <kondoumh@gmail.com>
2021-03-13 21:10:45 +0900

    Add cli version

---
+++ sd-202103-intro-wasm/main.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const wasm = require('./pkg-node/sd_202103_intro_wasm.js');
+
+const inPath = process.argv[2];
+const outPath = process.argv[3];
+
+const base64 = fs.readFileSync(inPath, 'base64');
+const data = base64.replace(/^data:image\/(png|jpg);base64,/, '');
+const img = wasm.PhotonImage.new_from_base64(data);
+const appliedFilter = wasm.apply_some_filter(img);
+console.log(`Applied filter: ${appliedFilter}`);
+const outData = img.get_base64().replace(/^data:image\/\w+;base64,/, '');
+fs.writeFileSync(outPath, outData, 'base64');

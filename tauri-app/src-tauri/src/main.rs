c2a318b2890efc65d310fabae07542003e6a84d4
kondoumh <kondoumh@gmail.com>
2022-03-06 11:12:12 +0900

    remove database

--- tauri-app/src-tauri/src/main.rs
+++ tauri-app/src-tauri/src/main.rs
@@ -4,19 +4,15 @@
 )]
 
 use std::{
-  collections::HashMap,
   sync::{
     atomic::{AtomicUsize, Ordering},
-    Arc, Mutex,
+    Mutex,
   },
 };
 
 use tauri::State;
 
 struct Counter(AtomicUsize);
-
-#[derive(Default)]
-struct Database(Arc<Mutex<HashMap<String, String>>>);
 
 struct Client;
 
@@ -54,25 +50,12 @@ fn increment_counter(counter: State<'_, Counter>) -> usize {
   counter.0.fetch_add(1, Ordering::Relaxed) + 1
 }
 
-#[tauri::command]
-fn db_insert(key: String, value: String, db: State<'_, Database>) {
-  db.0.lock().unwrap().insert(key, value);
-}
-
-#[tauri::command]
-fn db_read(key: String, db: State<'_, Database>) -> Option<String> {
-  db.0.lock().unwrap().get(&key).cloned()
-}
-
 fn main() {
   tauri::Builder::default()
     .manage(Counter(AtomicUsize::new(0)))
-    .manage(Database(Default::default()))
     .manage(Connection(Default::default()))
     .invoke_handler(tauri::generate_handler![
       increment_counter,
-      db_insert,
-      db_read,
       connect,
       disconnect,
       connection_send

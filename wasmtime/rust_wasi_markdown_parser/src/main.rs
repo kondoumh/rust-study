30b436367fa7857b326e6eee6404f3b2a9300c86
kondoumh <kondoumh@gmail.com>
2022-09-25 11:36:00 +0900

    update

--- wasmtime/rust_wasi_markdown_parser/src/main.rs
+++ wasmtime/rust_wasi_markdown_parser/src/main.rs
@@ -1,4 +1,4 @@
-// Import our CLI parsing libraries (And Pathbuf for reading path)
+// Import our CLI parsing libraries (And PathBuf for reading paths)
 extern crate structopt;
 
 use structopt::StructOpt;
@@ -7,24 +7,26 @@ use std::path::PathBuf;
 // Import our markdown parser library, crate
 extern crate pulldown_cmark;
 
+use pulldown_cmark::{html, Parser};
 // Import from the standard library, to allow reading from the file system
 use std::fs;
 
 // Define our CLI options using structopt
 #[derive(StructOpt)]
 #[structopt(name = "rust_wasi_markdown_parser", about = "Markdown to HTML renderer CLI, written with Rust & WASI")]
-pub struct Option {
-    // The markdown file to render
+pub struct Options {
+    /// The markdown file to render
+    #[structopt(parse(from_os_str))]
     filename: PathBuf,
 }
 
-// Out entrypoint into our WASI module
+// Our entrypoint into our WASI module
 fn main() {
     // Get the passed CLI options
     let options = Options::from_args();
     // Read the markdown file into a string
     let contents = fs::read_to_string(options.filename)
-        .expect("Someting went wrong reading the file");
+        .expect("Something went wrong reading the file");
     // Run our parsing function to get back an HTML string
     let result = render_markdown(contents);
     // Print out the resulting HTML to standard out
